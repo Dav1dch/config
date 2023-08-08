e605964d81662021145f7709e60557f58dc40af4
David <dav1dch@outlook.com>
2023-08-08 20:41:41 +0800

    migrate

--- .config/nvim_bak/lua/text.lua
+++
@@ -1,5 +0,0 @@
-local keymap = vim.keymap
-
-
-require("nvim-autopairs").setup {}
-keymap.set('n', '<space>ta', '<cmd>ToggleAlternate<cr>')
--- nvim/lua/plugins/p-lsp.lua
+++ nvim/lua/plugins/p-lsp.lua
@@ -1,5 +1,4 @@
 local M = {}
-
 M = {
   {
     'weilbith/nvim-code-action-menu',
@@ -33,13 +32,13 @@ M = {
         'j-hui/fidget.nvim',
         tag = "legacy"
       },
+      { 'L3MON4D3/LuaSnip' }, -- Required
       "folke/neodev.nvim",
       "ray-x/lsp_signature.nvim",
       {
         "lvimuser/lsp-inlayhints.nvim",
         branch = "anticonceal",
       },
-      -- "mjlbach/lsp_signature.nvim",
     },
 
     config = function()
@@ -49,7 +48,6 @@ M = {
       lsp.ensure_installed({
         'tsserver',
         'lua_ls',
-        'pylsp',
         'clangd'
 
       })
@@ -93,7 +91,7 @@ M = {
       local lspconfig = require('lspconfig')
 
       require("config.lsp.lua").setup(lspconfig, lsp)
-      require("config.lsp.pylsp").setup(lspconfig, lsp)
+      require("config.lsp.pyright").setup(lspconfig, lsp)
       require("config.lsp.clangd").setup(lspconfig, lsp)
 
       lsp.setup()
@@ -135,70 +133,6 @@ M = {
   },
 }
 
+
+
 return M
-
--- { 'williamboman/mason-lspconfig.nvim' },
--- { 'hrsh7th/cmp-nvim-lsp' },
-
-
--- {
---   "neovim/nvim-lspconfig",
---   config = function()
---     --
---     -- Set up lspconfig.
---     local capabilities = require("cmp_nvim_lsp").default_capabilities()
---     -- Setup language servers.
---     local lspconfig = require("lspconfig")
---     -- lspconfig.jedi_language_server.setup {
---     --   capabilities = capabilities,
---     -- }
---     lspconfig.pyright.setup({
---       capabilities = capabilities,
---     })
---     lspconfig.tsserver.setup({})
---     lspconfig.lua_ls.setup({
---       capabilities = capabilities,
---       settings = {
---         Lua = {
---           runtime = {
---             -- Tell the language server which version of Lua you're using
---             -- (most likely LuaJIT in the case of Neovim)
---             version = "LuaJIT",
---           },
---           diagnostics = {
---             -- Get the language server to recognize the `vim` global
---             globals = {
---               "vim",
---               "require",
---             },
---           },
---           workspace = {
---             -- Make the server aware of Neovim runtime files
---             library = vim.api.nvim_get_runtime_file("", true),
---             checkThirdParty = false,
---           },
---           -- Do not send telemetry data containing a randomized but unique identifier
---           telemetry = {
---             enable = false,
---           },
---         },
---       },
---     })
---
---     lspconfig.clangd.setup({
---       capabilities = capabilities,
---       settings = {
---         ["filetypes"] = { "c", "cpp", "objc", "objcpp", "cuda", "proto", "cc" },
---       },
---     })
---
---     lspconfig.rust_analyzer.setup({
---       -- Server-specific settings. See `:help lspconfig-setup`
---       capabilities = capabilities,
---       settings = {
---         ["rust-analyzer"] = {},
---       },
---     })
---   end,
--- },
--- }
